f19ee1359c5cd81672de4e8ddc3dbf1808effc18
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-07-13 05:01:59 +0600

    complete new DB

--- Zero_Hunger/Zero_Hunger/Models/NGO.cs
+++ Zero_Hunger/Zero_Hunger/Models/NGO.cs
@@ -7,8 +7,29 @@ using System.Web;
 
 namespace Zero_Hunger.Models
 {
-    public class NGO:User
-    {    
+    public class NGO
+    {
+        public int ID { get; set; }
+
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9._]{2,19}$", ErrorMessage = "Invalid username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(32)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Invalid password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Contract number is required")]
+        [RegularExpression(@"^01\d{9}$", ErrorMessage = "Invalid contract number")]
+        public string Contract { get; set; }
+
+        public string Address { get; set; }
 
         //----------------Relations--------------------------
 
