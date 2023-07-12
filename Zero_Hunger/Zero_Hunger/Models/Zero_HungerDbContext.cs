422c072bffaa9d2490dad384a68501edb096c79f
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-07-13 04:59:08 +0600

    Complete new database.

--- Zero_Hunger/Zero_Hunger/Models/Zero_HungerDbContext.cs
+++ Zero_Hunger/Zero_Hunger/Models/Zero_HungerDbContext.cs
@@ -10,7 +10,6 @@ namespace Zero_Hunger.Models
 {
     public class Zero_HungerDbContext : DbContext
     {
-        public DbSet<User> Users { get; set; }
         public DbSet<NGO> NGOs { get; set; }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Employee> Employees { get; set; }
@@ -19,31 +18,37 @@ namespace Zero_Hunger.Models
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            // NGO with restaurants
             modelBuilder.Entity<NGO>()
                 .HasMany(n => n.Restaurants)
                 .WithOptional()
-                .HasForeignKey(r => r.UserID);
+                .HasForeignKey(r => r.RestaurantID);
 
+            // NGO with Employee
             modelBuilder.Entity<NGO>()
                 .HasMany(n => n.Employees)
                 .WithOptional()
-                .HasForeignKey(e => e.UserID);
+                .HasForeignKey(e => e.EmpID);
 
+            // Restaurant with CollectionRequest
             modelBuilder.Entity<Restaurant>()
                 .HasMany(r => r.CollectionRequests)
                 .WithRequired(cr => cr.Restaurant)
                 .HasForeignKey(cr => cr.RestaurantId);
 
+            // Employee with collectionRequest
             modelBuilder.Entity<Employee>()
                 .HasMany(e => e.Collections)
                 .WithRequired(cr => cr.Employee)
                 .HasForeignKey(cr => cr.CollectRequestId);
 
+            // Employee with DistributionRecord
             modelBuilder.Entity<Employee>()
                 .HasMany(e => e.DistributionRecords)
                 .WithRequired(d => d.Employee)
                 .HasForeignKey(d => d.EmployeeID);
 
+            // collection request with distributionRecord
             modelBuilder.Entity<CollectRequest>()
                 .HasOptional(cr => cr.DistributionRecord)
                 .WithRequired(d => d.CollectRequest);
