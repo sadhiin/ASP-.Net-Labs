f3bbfa51bb27873da31586815efc0fd5e021b178
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-07-13 20:59:27 +0600

    Home controller UI + Logic ongoing.

--- Zero_Hunger/Zero_Hunger/Controllers/HomeController.cs
+++ Zero_Hunger/Zero_Hunger/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Zero_Hunger.Models;
 
 namespace Zero_Hunger.Controllers
 {
     public class HomeController : Controller
     {
+        private Zero_HungerDbContext _db;
         [Route("About")]
         public ActionResult Index()
         {
@@ -32,6 +34,7 @@ namespace Zero_Hunger.Controllers
         [HttpGet]
         public ActionResult NGOLogin()
         {
+            
             return View();
         }
 
@@ -39,8 +42,8 @@ namespace Zero_Hunger.Controllers
         [HttpPost]
         public ActionResult NGOLogin(NGO model)
         {
-            if (ModelState.IsValid)
-            {
+            //if (ModelState.IsValid)
+            //{
                 // Perform NGO login logic here
                 // Check the provided credentials and redirect to appropriate page
                 // Example: if (ValidNGOCredentials(model.UserName, model.Password))
@@ -50,9 +53,25 @@ namespace Zero_Hunger.Controllers
                 //           else
                 //           {
                 //               ModelState.AddModelError("", "Invalid credentials.");
-                //           }
-            }
+                //           
+            //}
+            _db = new Zero_HungerDbContext();
+            bool userExists = _db.NGOs.Any(
+                x => x.UserName.Equals(model.UserName) && 
+                x.Password.Equals(model.Password));
 
+            if (userExists == true)
+            {
+                NGO CurrentNGO = _db.NGOs.FirstOrDefault(
+                    x => x.UserName.Equals(model.UserName) &&
+                    x.Password.Equals(model.Password));
+
+                Session["Name"] = CurrentNGO.UserName;
+
+                FormsAuthentication.SetAuthCookie(CurrentNGO.UserName, false);
+                return RedirectToAction("Index", "NGODashboard");
+            }
+            ModelState.AddModelError("", "Username or Password is wrong");
             return View(model);
         }
 
@@ -66,21 +85,37 @@ namespace Zero_Hunger.Controllers
         [HttpPost]
         public ActionResult RestaurantLogin(Restaurant model)
         {
-            if (ModelState.IsValid)
-            {
-                // Perform restaurant login logic here
-                // Check the provided credentials and redirect to appropriate page
-                // Example: if (ValidRestaurantCredentials(model.UserName, model.Password))
-                //           {
-                //               return RedirectToAction("Dashboard", "Restaurant");
-                //           }
-                //           else
-                //           {
-                //               ModelState.AddModelError("", "Invalid credentials.");
-                //           }
-            }
+            //if (ModelState.IsValid)
+            //{
+            // Perform restaurant login logic here
+            // Check the provided credentials and redirect to appropriate page
+            // Example: if (ValidRestaurantCredentials(model.UserName, model.Password))
+            //           {
+            //               return RedirectToAction("Dashboard", "Restaurant");
+            //           }
+            //           else
+            //           {
+            //               ModelState.AddModelError("", "Invalid credentials.");
+            //           }
+            //}
 
-            return View(model);
+            _db = new Zero_HungerDbContext();
+
+            bool restaurentExists = _db.Restaurants.Any(
+                x => x.UserName.Equals(model.UserName) &&
+                x.Password.Equals(model.Password));
+
+            if (restaurentExists == true)
+            {
+                Restaurant restaurant = _db.Restaurants.FirstOrDefault(
+                    x => x.UserName.Equals(model.UserName) &&
+                    x.Password.Equals(model.Password));
+
+                Session["username"] = restaurant.UserName;
+                FormsAuthentication.SetAuthCookie(restaurant.UserName,false);
+            }
+            ModelState.AddModelError("", "Username or Password is wrong");
+            return View();
         }
 
         // GET: Home/RestaurantSignup
@@ -93,16 +128,32 @@ namespace Zero_Hunger.Controllers
         [HttpPost]
         public ActionResult RestaurantSignup(Restaurant model)
         {
-            if (ModelState.IsValid)
+            //if (ModelState.IsValid)
+            //{
+            // Perform restaurant signup logic here
+            // Create a new Restaurant object and save it to the database
+            // Example: Restaurant restaurant = new Restaurant { RestaurantName = model.RestaurantName, UserName = model.UserName, Password = model.Password, Location = model.Location, Contract = model.Contract };
+            //           SaveRestaurant(restaurant);
+            //           return RedirectToAction("RestaurantLogin");
+            //}
+            _db = new Zero_HungerDbContext();
+
+            bool Exists = _db.Restaurants.Any(
+                x => x.UserName.Equals(model.UserName));
+            if(Exists == true)
             {
-                // Perform restaurant signup logic here
-                // Create a new Restaurant object and save it to the database
-                // Example: Restaurant restaurant = new Restaurant { RestaurantName = model.RestaurantName, UserName = model.UserName, Password = model.Password, Location = model.Location, Contract = model.Contract };
-                //           SaveRestaurant(restaurant);
-                //           return RedirectToAction("RestaurantLogin");
+                ViewBag.UserExists = "User already exists. Try to login";
+                return View();
+            }
+            else
+            {
+                _db.Restaurants.Add(model);
+                _db.SaveChanges();
+
+                return RedirectToAction("RestaurantLogin");
             }
 
-            return View(model);
+            return View();
         }
 
         // GET: Home/EmployeeLogin
@@ -128,7 +179,6 @@ namespace Zero_Hunger.Controllers
                 //               ModelState.AddModelError("", "Invalid credentials.");
                 //           }
             }
-
             return View(model);
         }
     }
---
+++ Zero_Hunger/Zero_Hunger/Controllers/NGODashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zero_Hunger.Controllers
+{
+    public class NGODashboardController : Controller
+    {
+        // GET: NGODashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
